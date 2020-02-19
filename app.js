33515f71c0878a895075ceadda630b3457b67316
MeRichard123 <rcoric03@gmail.com>
2020-02-19 16:04:31 +0000

    Fixed navigation

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const navigation = document.querySelector("nav");
 
 window.addEventListener("load", () => {
-  window.addEventListener("scroll", () => {
+  setInterval(reset, 100);
+});
+
+reset = function() {
+  if (scrollY == 0) {
+    navigation.style.setProperty("background", "rgba(0, 0, 0, 1)");
+  } else {
     navigation.style.setProperty("background", "rgba(0, 0, 0, .8)");
-  });
+  }
+};
+
+AOS.init({
+  offset: 400,
+  duration: 1000
 });
