a60651f5eea539f1eca91be804de65b894588069
MeRichard123 <rcoric03@gmail.com>
2020-02-20 12:18:42 +0000

    Font Size Edit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const navigation = document.querySelector("nav");
+const checkbox = document.querySelector("input[name=theme]");
 
 window.addEventListener("load", () => {
   setInterval(reset, 100);
@@ -10,6 +11,22 @@ reset = function() {
   } else {
     navigation.style.setProperty("background", "rgba(0, 0, 0, .8)");
   }
+};
+
+checkbox.addEventListener("change", function() {
+  if (this.checked) {
+    trans();
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    trans();
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+});
+let trans = () => {
+  document.documentElement.classList.add("transition");
+  window.setTimeout(() => {
+    document.documentElement.classList.remove("transition");
+  }, 1000);
 };
 
 AOS.init({
